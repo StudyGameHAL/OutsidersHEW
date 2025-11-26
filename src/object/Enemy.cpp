beae78bdea1c7169cd430e64901b4881c9b81665
tankerbibi <towakawaguti@gmail.com>
2025-11-26 22:20:45 +0900

    AddEnemyPlayerRotationSystem

--- src/object/Enemy.cpp
+++ src/object/Enemy.cpp
@@ -7,13 +7,27 @@ class Player;
 void Enemy::Update()
 {
 	Scene* scene = GetScene();
+
 	// GameObject* player = scene->GetGameObject<Player>();
 	GameObject* player;
+
+	// プレイヤーのベクトルを取得
 	XMVECTOR playerPosVec = player->GetTransform().GetPosition().ToXMVECTOR();
+	// 自分のベクトルを取得
 	XMVECTOR posVec = player->GetTransform().GetPosition().ToXMVECTOR();
 
+	// プレイヤーの方向を向くマトリクスを取得
 	XMMATRIX matrix = XMMatrixLookAtLH(posVec, playerPosVec, XMVectorSet(0.0f, 1.0f, 0.0f));
 
+	XMVECTOR rotation = GetTransform().GetRotationEuler().ToXMVECTOR();
+
+	XMVECTOR newRotation = XMVector3TransformNormal(rotation, matrix);
+
+	m_Transform.SetRotationEuler(Vector3(
+		XMVectorGetX(newRotation),
+		XMVectorGetY(newRotation),
+		XMVectorGetZ(newRotation)
+	));
 
 }
 
