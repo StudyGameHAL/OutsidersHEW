ea845fadce0047a5da87eb5d42547f95c431c1d7
laukachuen <laukachuen@gmail.com>
2025-11-30 22:32:49 +0900

    Projectile.h .cpp added

---
+++ src/object/Projectile.cpp
@@ -0,0 +1,29 @@
+#include "Projectile.h"
+
+Projectile::Projectile()
+{
+}
+
+Projectile::~Projectile()
+{
+}
+
+void Projectile::Initialize()
+{
+}
+
+void Projectile::Update()
+{
+}
+
+void Projectile::Draw()
+{
+}
+
+void Projectile::SetVelocity(const Vector3& vel)
+{
+}
+
+void Projectile::SetAttackPower(int power)
+{
+}
---
+++ src/object/Projectile.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "GameObject.h"
+
+class Projectile : public GameObject
+{
+public:
+	Projectile();
+	~Projectile();
+
+	void Initialize() override;
+	void Update() override;
+	void Draw() override;
+
+	// setter
+	void SetVelocity(const Vector3& vel);
+	void SetAttackPower(int power);
+
+private:
+	Vector3 m_Velocity = { 0,0,0 };
+	int m_AttackPower = 1;
+
+
+
+};
