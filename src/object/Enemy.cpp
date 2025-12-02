7ec3ec2be6b1f5daab4c049a68171b2bd8e6a46c
tankerbibi <166798898+tankerbibi@users.noreply.github.com>
2025-12-02 09:20:50 +0900

    AddEnemyShootingSystem

--- src/object/Enemy.cpp
+++ src/object/Enemy.cpp
@@ -3,17 +3,35 @@
 #include "scene/Scene.h"
 
 class Player;
+class Projectile;
 
 void Enemy::Update()
 {
 	Scene* scene = GetScene();
+
 	// GameObject* player = scene->GetGameObject<Player>();
 	GameObject* player;
+
 	XMVECTOR playerPosVec = player->GetTransform().GetPosition().ToXMVECTOR();
 	XMVECTOR posVec = player->GetTransform().GetPosition().ToXMVECTOR();
 
-	XMMATRIX matrix = XMMatrixLookAtLH(posVec, playerPosVec, XMVectorSet(0.0f, 1.0f, 0.0f));
+	XMVECTOR forwardVec = XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
+	XMVECTOR upVec = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	XMMATRIX matrix = XMMatrixLookAtLH(posVec, playerPosVec, upVec);
 
+	// アタックカウントを加算
+	m_AttackCount++;
+
+	// アタックするカウントになったとき
+	if (m_CountToAttack)
+	{
+		// シーンにゲームオブジェクトを追加
+		scene->AddGameObject<Projectile>();
+
+		// プロジェクタイルに力を加える
+
+
+	}
 
 }
 
--- src/object/Enemy.h
+++ src/object/Enemy.h
@@ -5,10 +5,15 @@
 class Enemy : public GameObject
 {
 private:
+
+private:
 	Vector3 m_Velocity = Vector3(0.0f, 0.0f, 0.0f);
 	int m_Health = 0;
 	Vector3 m_MaxMoveSpeed = Vector3(0.0f, 0.0f, 0.0f);
+	int m_AttackCount = 0;
 
+private:
+	static constexpr int m_CountToAttack = 120;
 public:
 	Enemy() {};
 	virtual ~Enemy() {};
