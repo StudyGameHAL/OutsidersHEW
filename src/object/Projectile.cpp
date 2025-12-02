08dcd23be99861af985adb25520d0a32449c7f2f
laukachuen <laukachuen@gmail.com>
2025-12-02 10:58:00 +0900

    updated Projectile.cpp ( Initialize, Update, setVelocity, SetAttackPower)

--- src/object/Projectile.cpp
+++ src/object/Projectile.cpp
@@ -1,5 +1,6 @@
 #include "Projectile.h"
-
+#include <chrono>
+#include "Game.h"
 Projectile::Projectile()
 {
 }
@@ -10,10 +11,29 @@ Projectile::~Projectile()
 
 void Projectile::Initialize()
 {
+	m_Lifetime = 2.0f;
 }
 
 void Projectile::Update()
 {
+	static auto old = std::chrono::steady_clock::now();
+	auto now = std::chrono::steady_clock::now();
+
+	std::chrono::duration<float> delta = now - old;
+	old = now;
+
+	float dt = delta.count();
+
+	// Move projectile
+	auto pos = m_Transform.GetPosition();
+	pos = pos + m_Velocity * dt;
+
+	// Reduce lifetime
+	m_Lifetime -= dt;
+	if (m_Lifetime <= 0.0f)
+	{
+		// destroy ?
+	}
 }
 
 void Projectile::Draw()
@@ -22,8 +42,10 @@ void Projectile::Draw()
 
 void Projectile::SetVelocity(const Vector3& vel)
 {
+	m_Velocity = vel;
 }
 
 void Projectile::SetAttackPower(int power)
 {
+	m_AttackPower = power;
 }
